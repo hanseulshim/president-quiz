aa10482f4c68aa89f62a03f712d293d044d54e0f
hanseulshim <hanseulshim@gmail.com>
2017-05-24 11:10:17 -0500

    add noreferrer to links

--- src/components/social.js
+++ src/components/social.js
@@ -6,13 +6,13 @@ class Social extends Component {
   render() {
     return (
       <div className="col-md-12 col-sm-12 col-xs-12">
-        <a target="_blank" href="https://www.facebook.com/">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/">
           <button type="button" className="btn btn-primary col-md-1 col-sm-2 col-xs-2 social">
             <img src={facebook} alt="Facebook"/>
             (100)
           </button>
         </a>
-        <a target="_blank" href="https://twitter.com/home?status=Take%20this%20quiz%20to%20see%20which%20republican%20president%20you%20are!">
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/home?status=Take%20this%20quiz%20to%20see%20which%20republican%20president%20you%20are!">
           <button type="button" className="btn btn-info col-md-1 col-sm-2 col-xs-2 social">
             <img src={twitter} alt="Twitter"/>
             (100)
--- src/components/socialResult.js
+++ src/components/socialResult.js
@@ -7,13 +7,13 @@ class SocialResult extends Component {
     return (
       <div className="col-md-12 col-sm-12 col-xs-12">
         <h2>Share your results!</h2>
-        <a target="_blank" href="https://www.facebook.com/">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/">
           <button type="button" className="btn btn-primary col-md-1 col-sm-2 col-xs-2 social">
             <img src={facebook} alt="Facebook"/>
             (100)
           </button>
         </a>
-        <a target="_blank" href= {"https://twitter.com/home?status=I%20%am%20"+this.props.name}>
+        <a target="_blank" rel="noopener noreferrer" href= {"https://twitter.com/home?status=I%20%am%20"+this.props.name}>
           <button type="button" className="btn btn-info col-md-1 col-sm-2 col-xs-2 social">
             <img src={twitter} alt="Twitter"/>
             (100)
